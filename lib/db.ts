a79b880dec46ebd539dc4d550a108667de0302e0
Bharat Khatwani <bharatkhatwani06@gmail.com>
2025-08-26 05:53:28 +0000

    'database added'

--- lib/db.ts
+++ lib/db.ts
@@ -4,9 +4,8 @@ const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
 export const db =
   globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ["query", "error", "warn"], // optional for debugging
-  });
+new PrismaClient()
+   
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = db;
